4d67169fc0aea673909a74e9ded4c4e7dcb89e7a
MOHAMMAD ANSHID <anshidmohammad42@gmail.com>
2025-02-22 16:52:11 +0530

    last onedfxddx

--- server.js
+++ server.js
@@ -118,7 +118,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }))
 app.use(
   cors({
-    origin:"https://master.d37rsas8lcxh5s.amplifyapp.com", 
+    origin:"https://master.d298fqlts9wdsx.amplifyapp.com", 
     credentials: true, 
     methods: ["GET", "POST", "PUT", "DELETE"], // Allowed request methods
     allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
@@ -155,7 +155,7 @@ app.use((err, req, res, next) => {
 
 const io = new Server(server, {
   cors: {
-    origin:"https://master.d37rsas8lcxh5s.amplifyapp.com",
+    origin:"https://master.d298fqlts9wdsx.amplifyapp.com",
     credentials: true,
   },
 });
